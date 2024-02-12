d9369634c878545e297da76f9f26191b77cbef12
Oscar García <garcia.oscar1857@gmail.com>
2024-02-11 19:34:58 -0600

    Commit Resta

--- Calculator.c
+++ Calculator.c
@@ -10,6 +10,7 @@ int main (){
     int a=0;
     int b=0;
     int sum=0;
+    int res=0;
     int opc=0;
     printf("Elige la opción:\n");
     printf("1. Suma\n");
@@ -25,6 +26,9 @@ int main (){
         printf("El resultado de la suma es: %d",sum); 
         break;
         case 2:
+        scanf("%i %i",&a,&b);
+        res=resta(a,b);
+        printf("El resultado de la suma es: %d",res); 
         break;
         case 3:
         break;
@@ -39,4 +43,7 @@ int main (){
 
 int suma(int a, int b){
     return a+b;
+}
+int resta(int a, int b){
+    return a-b;
 }
